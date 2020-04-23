991c2b1a59740a408dbc1b4f99b09fae9eb4433a
tkrzewina <59062750+tkrzewina@users.noreply.github.com>
2020-04-23 13:44:20 +0100

    Add files via upload

---
+++ 200423sexshufflev1.py
@@ -0,0 +1,12 @@
+import random 
+sex_acts = ["anal","bj","doggy","69","fisting","cannilingus","lick balls","piss"]
+print("Welcome to the porn script generator")
+random.shuffle(sex_acts)
+scene1=sex_acts[0:4]
+print("Scene 1")
+for sex_acts in scene1:
+    print(sex_acts)
+print("Scene 2")
+scene2=sex_acts[4:8]
+for sex_acts in scene2:
+    print(sex_acts)
