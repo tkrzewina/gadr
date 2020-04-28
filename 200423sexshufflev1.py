9beb9f9a04479b3ac2443973aea279591a873688
tkrzewina <59062750+tkrzewina@users.noreply.github.com>
2020-04-28 11:26:53 +0100

    Update 200423sexshufflev1.py

--- 200423sexshufflev1.py
+++ 200423sexshufflev1.py
@@ -4,9 +4,11 @@ print("Welcome to the porn script generator")
 random.shuffle(sex_acts)
 scene1=sex_acts[0:4]
 print("Scene 1")
-for sex_acts in scene1:
-    print(sex_acts)
+for sex_acts1 in scene1:
+    print(sex_acts1)
+print("Orgasm happend during " + random.choice(scene1))
 print("Scene 2")
 scene2=sex_acts[4:8]
-for sex_acts in scene2:
-    print(sex_acts)
+for sex_acts2 in scene2:
+    print(sex_acts2)
+print("Orgasm happend during " + random.choice(scene2))
